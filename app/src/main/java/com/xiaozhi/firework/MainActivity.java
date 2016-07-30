82932e62cc713a0030c971b8e8a5df47d12dfa29
JingTianXiaoZhi <1243439709@qq.com>
2016-07-30 21:37:09 +0800

    add add example

--- app/src/main/java/com/xiaozhi/firework/MainActivity.java
+++ app/src/main/java/com/xiaozhi/firework/MainActivity.java
@@ -1,47 +1,24 @@
 package com.xiaozhi.firework;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 
 import com.xiaozhi.firework_core.FireWorkView;
 
-public class MainActivity extends Activity {
-	private FireWorkView fireWorkView;
-	private LinearLayout linearlayout;
+public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		linearlayout = (LinearLayout) findViewById(R.id.linearlayout);
-	}
-
-	public void addFlowerView(View view) {
-		if(fireWorkView == null){
-			fireWorkView = new FireWorkView(this, R.drawable.rose);
-			addFireView();
-		}
-	}
-
-	public void addHeartView(View view) {
-		if(fireWorkView == null){
-			fireWorkView = new FireWorkView(this, R.drawable.heart);
-			addFireView();
-		}
-	}
-
-	private void addFireView() {
-		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+		LinearLayout linearlayout = (LinearLayout) findViewById(R.id.linearlayout);
+		//1.新建一个烟花效果(第二个参数是礼物资源Id)
+		FireWorkView fireWorkView = new FireWorkView(MainActivity.this, R.mipmap.ic_launcher);
+		//2.在布局上增加烟花效果
+		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(-1, -1);
 		linearlayout.addView(fireWorkView, layoutParams);
+		//3.播放动画
 		fireWorkView.playAnim();
 	}
-
-	public void removeAnimal(View view) {
-		linearlayout.removeView(fireWorkView);
-		fireWorkView.stopAnim();
-		fireWorkView = null;
-	}
-
 }
